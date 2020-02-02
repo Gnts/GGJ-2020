5e55138c28336784cb47858096fd36c465d5bb45
Gintautas Skersys <gintautas@unity3d.com>
2020-02-02 11:33:29 +0200

    Gmeplay scene

---
+++ project/Assets/Fly.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+    Rigidbody _rb;
+    
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.AddForce(Vector3.forward*500f);
+        _rb.AddForce(Vector3.up*200f);
+    }
+}
